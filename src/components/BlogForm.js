395f0ccd2e570dff5fa06fd0e9939c72880c0c15
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-21 14:12:54 +0300

    Minor code refactor

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import blogsService from '../services/blogs'
 
 
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import styles from './notification.module.css'
 
 const Notification = ({text, color}) => {
